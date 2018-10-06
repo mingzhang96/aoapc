5aa9cf82e9c4f344fedf24729820d103f7ec006e
zhangming <389157423@qq.com>
2018-10-06 16:29:03 +0800

    2-1

--- chapter_2/exercise/2-1.c
+++ chapter_2/exercise/2-1.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
 
+/*
+输出100-999中的水仙花数
+*/
 int main() {
+	int num1, num10, num100;
+	for (int i = 100; i <= 999; i++) {
+		num1 = i % 10;
+		num10 = i / 10 % 10;
+		num100 = i / 100 % 10;
+		if (i == num1*num1*num1 + num10*num10*num10 + num100*num100*num100) {
+			printf("%d ", i);
+		}
+	}
 	return 0;
 }
